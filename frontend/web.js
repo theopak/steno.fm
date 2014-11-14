883496b90c2e04ea13bd374e6c643762575670f9
Theodore X. Pak <mail@theopak.com>
2014-11-14 01:54:34 -0800

    Adds `Access-Control-Allow-Origin` to Express server.

--- frontend/web.js
+++ frontend/web.js
@@ -1,7 +1,15 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
  
-app.use(express.logger('dev'));
+// app.use(express.logger('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+app.use(allowCrossDomain);
+app.listen(process.env.PORT || 80);
