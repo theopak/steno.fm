c42fb1853c0a0240bbb3e3ce8458e709877cc557
Trevor Phillippi <trevor@tjphilli.com>
2014-11-13 21:28:54 -0800

    Adds the GlobalUiService.js file that I forgot

---
+++ frontend/app/scripts/services/GlobalUIService.js
@@ -0,0 +1,25 @@
+'use strict';
+// Service to store and retrieve application wide data
+app.factory('GlobalUi', [function(){
+	var Globals = {
+		hidden_status: true,
+		isHiddenChrome: function() {
+			return this.hidden_status;
+		},
+		// getStatus: function	() {
+		// 	// alert('working');
+		// 	return this.isHiddenChrome;
+		// },
+		showChrome: function() {
+			this.hidden_status = false;
+		},
+		hideChrome: function() {
+			this.hidden_status = true;
+		},
+		toggleChrome: function() {
+			console.log("working");
+			this.hidden_status = !this.hidden_status;
+		}
+	};
+	return Globals;
+}]);
