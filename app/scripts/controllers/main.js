c34e7b207deca04979021389f4add33dc3f9874b
Theodore X. Pak <mail@theopak.com>
2014-11-13 17:12:15 -0800

    Adds and uses `angular-foundation` on async example. Refer to http://pineconellc.github.io/angular-foundation/#/typeahead

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,10 +7,24 @@
  * # MainCtrl
  * Controller of the stenoApp
  */
-app.controller('MainCtrl', function ($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
+app.controller('MainCtrl', function ($scope, $http) {
+  
+  $scope.selected = undefined;	// model (angular expression) assigned to
+  $scope.isLoading = false;			// true if the typeahead is asyncronously loading results
+  // Any function returning a promise object can be used to load values asynchronously
+  $scope.getLocation = function(val) {
+    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+      params: {
+        address: val,
+        sensor: false
+      }
+    }).then(function(res){
+      var addresses = [];
+      angular.forEach(res.data.results, function(item){
+        addresses.push(item.formatted_address);
+      });
+      return addresses;
+    });
+  };
+
 });
