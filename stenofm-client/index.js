581eba8c2862674d0abc690cb9d8855f60f347c5
Theodore X. Pak <theopak@gmail.com>
2015-11-23 22:51:31 -0500

    Start over by cloning https://github.com/rackt/redux/tree/master/examples/real-world

--- stenofm-client/index.js
+++ stenofm-client/index.js
@@ -1,40 +1,12 @@
-var gzippo = require('gzippo');
-var express = require('express');
+import 'babel-core/polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
 
-/**
- * app
- */
-var app = express();
-module.exports = app;
+const store = configureStore()
 
-// Emit errors as events to a log
-app.on('after', function(req, resp, route, err) {
-  if (err) {
-    console.error(err);
-  }
-});
-
-// Enable CORS for development purposes, but not in production
-if (process.env.NODE_ENV != 'production') {
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    next();
-  });
-  console.log('CORS allowed (this option is enabled when node process.env is not \'production\').');
-}
-
-// Register handlers to execute when app is closing
-process.on('exit', function(){console.log('(exit pid %d)', process.pid)});
-
-// Frontend
-app.use('/', express.static(__dirname + '/dist'));
-
-// Start server
-app.listen(process.env.PORT || 9000, function(err) {
-  console.log('\n===== express.js app (pid %d) =====\n%s\n', process.pid, __dirname);
-  if (err) {
-    console.error(err);
-  }
-});
+render(
+  <Root store={store} />,
+  document.getElementById('root')
+)
